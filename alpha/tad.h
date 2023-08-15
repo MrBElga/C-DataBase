63d531cdc52f866a9d658bfca8e43acbb27631c3
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2023-08-15 17:16:30 -0300

    structs

--- alpha/tad.h
+++ alpha/tad.h
@@ -1,13 +1,53 @@
 
+// Union data type
+// I INTEGER
+// N NUMERIC
+// D DATE (aaaa-mm-dd)
+// C CHARACTER(1)
+// T CHARACTER(20)
 
-struct database
+union UDados
 {
-    
+    int ValorI;
+    float ValorN;
+    char valorD[11], ValorC, ValorT[21];
 };
-typedef database DataBase;
 
-struct 
+struct Pdados
 {
-    /* data */
+    union UDados UDados;
+    struct Pdados *prox;
 };
-typedef ;
+
+struct Patual{
+    struct Pdados *Pdados;
+}; 
+typedef struct Patual Patual;
+
+struct PCampos
+{
+    char Campo[30], Tipo, PK;
+    struct Patual *FK;
+    struct Pdados *ValorT;
+    struct PCampos *Patual, *prox;
+}; 
+typedef struct PCampos PCampos;
+
+
+struct PTabelas
+{
+    char Tabela[30];
+    struct PCampos *Patual;
+    struct PTabelas *prox, *ant;
+};
+typedef struct PTabelas PTabelas;
+
+struct pontBD
+{
+    char Banco_Dados[50];
+    struct pontBD *PTabelas;
+};
+typedef struct pontBD pontBD;
+
+
+

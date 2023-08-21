2deb42c000902621fb40c4b4fc1a2d2b7353a57d
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2023-08-21 16:09:47 -0300

    exibir + cad 1/2

--- alpha/main.c
+++ alpha/main.c
@@ -4,11 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "tad.h"
 
 int main()
 {
+    PDados *dados = NULL;
+    CadastrarDados(&dados, (union UDados){.ValorI = 42});
+    CadastrarDados(&dados, (union UDados){.ValorN = 3.14});
 
-    
+    PCampos *campos = NULL;
+    CadastrarCampos(&campos, "Nome", 'C', 'N');
+    CadastrarCampos(&campos, "Idade", 'I', 'N');
+
+    PTabelas *tabelas = NULL;
+    CadastrarTabela(&tabelas, "Pessoas");
+    CadastrarTabela(&tabelas, "Produtos");
+
+    printf("Dados cadastrados:\n");
+    ExibirDados(dados);
+
+    printf("\nCampos cadastrados:\n");
+    ExibirCampos(campos);   
     return 0;
 }
--- alpha/tad.h
+++ alpha/tad.h
@@ -1,15 +1,9 @@
-// Union data type
-// I INTEGER
-// N NUMERIC
-// D DATE (aaaa-mm-dd)
-// C CHARACTER(1)
-// T CHARACTER(20)
-
 union UDados
 {
     int ValorI;
     float ValorN;
-    char valorD[11], ValorC, ValorT[21];
+    char ValorD[11], ValorC;
+    char ValorT[21];
 };
 
 struct PDados
@@ -29,7 +23,7 @@ struct PCampos
 {
     char Campo[30], Tipo, PK;
     struct PAtual *FK;
-    struct Pdados *ValorT;
+    struct PDados *ValorT;
     struct PCampos *PAtual, *prox;
 };
 typedef struct PCampos PCampos;
@@ -45,23 +39,27 @@ typedef struct PTabelas PTabelas;
 struct pontBD
 {
     char Banco_Dados[50];
-    struct pontBD *PTabelas;
+    struct PTabelas *PTabelas;
 };
 typedef struct pontBD pontBD;
 
-
-//cadastro Dados
-Pdados *novaCaixaDados()
+PDados *novaCaixaDados()
 {
-    PDados *nova = (PDados*)malloc(sizeof(PDados));
+    PDados *nova = (PDados *)malloc(sizeof(PDados));
+    nova->UDados.ValorI = 0;
+    nova->UDados.ValorN = 0.0;
+    nova->UDados.ValorC = '\0';
+    nova->UDados.ValorD[0] = '\0';
+    nova->UDados.ValorT[0] = '\0';
     nova->prox = NULL;
     return nova;
 }
 
+
 void CadastrarDados(PDados **pDados, union UDados nDado)
 {
     PDados *novo = novaCaixaDados();
-    novo->Udados = nDado;
+    novo->UDados = nDado;
 
     if (*pDados == NULL)
     {
@@ -69,42 +67,122 @@ void CadastrarDados(PDados **pDados, union UDados nDado)
     }
     else
     {
-        //deve inserir no final não no incio
-        novo->prox = *pDados;
-        *pDados = novo;
+        PDados *temp = *pDados;
+        while (temp->prox != NULL)
+        {
+            temp = temp->prox;
+        }
+        temp->prox = novo;
     }
 }
 
-//Excluir Dados
-
-//Exibir Dados
-
-//Cadastrar Campos
-pCampos *novoCaixaCampo(char nome[], char Tipo,char FK)
+PCampos *novoCaixaCampo(char nome[], char Tipo, char FK)
 {
-    PCampos *nova =(PCampos*)malloc(sizeof(PCampos));
-    nova -> prox =  nova -> FK =  nova -> PAtual =   nova -> PDados =NULL;
-    novo -> Tipo = Tipo;
-    novo -> FK = FK;
-    strcpy(novo -> Campo, nome);
-    return(nova);
+    PCampos *nova = (PCampos *)malloc(sizeof(PCampos));
+    nova->prox = nova->FK = nova->PAtual = nova->ValorT = NULL;
+    nova->Tipo = Tipo;
+    nova->PK = FK;
+    strcpy(nova->Campo, nome);
+    return nova;
 }
 
-void CadastrarCampos(PCampos **PCampos,char nome[], char Tipo,char FK)
+void CadastrarCampos(PCampos **pCampos, char nome[], char Tipo, char FK)
 {
-    PCampos *novo = novoCaixaCampo(nome,Tipo, FK);
-    if(*PCampos == NULL)
+    PCampos *novo = novoCaixaCampo(nome, Tipo, FK);
+    if (*pCampos == NULL)
     {
-        *PCampos = novo;
+        *pCampos = novo;
     }
     else
     {
-
+        PCampos *temp = *pCampos;
+        while (temp->prox != NULL)
+        {
+            temp = temp->prox;
+        }
+        temp->prox = novo;
     }
 }
 
-//Cadastro Tabelas
-void CadastrarTabela()
+PTabelas *novaTabela(char nome[])
 {
+    PTabelas *nova = (PTabelas *)malloc(sizeof(PTabelas));
+    strcpy(nova->Tabela, nome);
+    nova->Patual = NULL;
+    nova->prox = nova->ant = NULL;
+    return nova;
+}
 
+void CadastrarTabela(PTabelas **Tabela, char nome[])
+{
+    PTabelas *nova = novaTabela(nome);
+    if (*Tabela == NULL)
+    {
+        *Tabela = nova;
+    }
+    else
+    {
+        PTabelas *temp = *Tabela;
+        while (temp->prox != NULL)
+        {
+            temp = temp->prox;
+        }
+        temp->prox = nova;
+        nova->ant = temp;
+    }
+}
+
+
+
+void ExibirDados(PDados *pDados)
+{
+    PDados *atual = pDados;
+    while (atual != NULL)
+    {
+        printf("Tipo do dado: ");
+
+        if (atual->UDados.ValorI > 0) {
+            printf("INTEGER\n");
+            printf("Valor: %d\n", atual->UDados.ValorI);
+        }
+        else if (atual->UDados.ValorN > 0) {
+            printf("NUMERIC\n");
+            printf("Valor: %.2f\n", atual->UDados.ValorN);
+        }
+        else if (strlen(atual->UDados.ValorD) > 0) {
+            printf("DATE\n");
+            printf("Valor: %s\n", atual->UDados.ValorD);
+        }
+        else if (atual->UDados.ValorC != '\0') {
+            printf("CHARACTER\n");
+            printf("Valor: %c\n", atual->UDados.ValorC);
+        }
+        else if (strlen(atual->UDados.ValorT) > 0) {
+            printf("CHARACTER (20)\n");
+            printf("Valor: %s\n", atual->UDados.ValorT);
+        }
+        else {
+            printf("NULO\n");
+        }
+
+        printf("\n");
+
+        atual = atual->prox;
+    }
+}
+
+
+
+
+void ExibirCampos(PCampos *pCampos)
+{
+    PCampos *atual = pCampos;
+    while (atual != NULL)
+    {
+        printf("Campo: %s\n", atual->Campo);
+        printf("Tipo: %c\n", atual->Tipo);
+        printf("PK: %c\n\n", atual->PK);
+
+        atual = atual->prox;
+    }
 }
